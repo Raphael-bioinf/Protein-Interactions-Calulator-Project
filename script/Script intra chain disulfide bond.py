16c2c30bdfec69b6de30842fca03f4161f836696
Raphael-bioinf <70586346+Raphael-bioinf@users.noreply.github.com>
2020-09-12 15:38:53 +0200

    Update Script intra chain disulfide bond.py

--- script/Script intra chain disulfide bond.py
+++ script/Script intra chain disulfide bond.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-prot_id = "5AGY.pdb"
-prot_file = "5AGY.pdb"
+prot_id = "1eej.pdb"
+prot_file = "1eej.pdb"
 
 from Bio.PDB import PDBParser
 parser=PDBParser(PERMISSIVE=1)
@@ -40,6 +37,47 @@ for c3 in cysresiduesB:
                 print ("Cys",c3index,"-")
                 print ("Cys",c4index,)
                 print (round(c3['SG'] - c4['SG'],2))
+                
+for c1 in cysresiduesA:
+    c1index = c1.get_id()[1]
+    for c2 in cysresiduesA:
+        c2index = c2.get_id()[1]
+        for c3 in cysresiduesB:
+                c3index = c3.get_id()[1]
+                for c4 in cysresiduesB:
+                        c4index = c4.get_id()[1]
+                        if(c1index != c2index):
+                            if(c1['SG'] - c2['SG'])< 2.2:
+                                print("possible di-sulfide bond in chain A:"),
+                                print ("Cys",c1index,"-")
+                                print ("Cys",c2index,)
+                                print (round(c1['SG'] - c2['SG'],2))
+                        if(c3index != c4index):
+                            if (c3['SG'] - c4['SG'])< 2.2:
+                                print("possible di-sulfide bond in chain B:"),
+                                print ("Cys",c3index,"-")
+                                print ("Cys",c4index,)
+                                print (round(c3['SG'] - c4['SG'],2))
+                        if(c1['SG'] - c3['SG'])< 2.2:
+                            print("possible di-sulfide bond between chain A and B:"),
+                            print ("Cys",c1index,"-")
+                            print ("Cys",c3index,)
+                            print (round(c1['SG'] - c3['SG'],2))
+                        if(c2['SG'] - c4['SG'])< 2.2:
+                            print("possible di-sulfide bond between chain A and B:"),
+                            print ("Cys",c2index,"-")
+                            print ("Cys",c4index,)
+                            print (round(c2['SG'] - c4['SG'],2))
+                        if(c1['SG'] - c4['SG'])< 2.2:
+                            print("possible di-sulfide bond between chain A and B:"),
+                            print ("Cys",c1index,"-")
+                            print ("Cys",c4index,)
+                            print (round(c1['SG'] - c4['SG'],2))
+                        if(c2['SG'] - c3['SG'])< 2.2:
+                            print("possible di-sulfide bond between chain A and B:"),
+                            print ("Cys",c2index,"-")
+                            print ("Cys",c3index,)
+                            print (round(c2['SG'] - c3['SG'],2))
 
 
 
