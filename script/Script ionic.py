4a5558fa9fb11f5c4c6bf363b436bb81ac97b90f
Léo Maury <leomaury97@gmail.com>
2020-09-14 09:49:57 +0200

    add 'or' operations

--- script/Script ionic.py
+++ script/Script ionic.py
@@ -16,15 +16,15 @@ bchain = model['B']
     
 ionicres = []
 for residue in achain:
-    if residue.get_resname() == "ARG":
+    if (residue.get_resname() == "ARG") or (residue.get_resname() == "LYS") or (residue.get_resname() == "HIS") or (residue.get_resname() == "ASP") or (residue.get_resname() == "GLU"):
         ionicres.append(residue)
 for c1 in ionicres:
     c1index = c1.get_id()[1]
     for c2 in ionicres:
         c2index = c2.get_id()[1]
         if(c1['SG'] - c2['SG'])< 2.2:
-            print("possible di-sulfide bond:"),
-            print ("Cys",c1index,"-")
+            print("possible ionic bond:"),
+            print ("Arg",c1index,"-")
             print ("Cys",c2index,)
             print (round(c1['SG'] - c2['SG'],2))
 
