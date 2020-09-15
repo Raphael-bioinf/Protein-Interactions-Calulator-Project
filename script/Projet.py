f26330760d300a2e22778a9048cb0a99e81902a1
Léo Maury <leomaury97@gmail.com>
2020-09-15 11:56:44 +0200

    minor changes

--- script/Projet.py
+++ script/Projet.py
@@ -8,10 +8,71 @@
 prot_id = "5AGY.pdb"
 prot_file = "../data/5AGY.pdb"
 
+
+
+amino = {
+    "hydrophobic": [
+        "ALA",
+        "VAL",
+        "LEU",
+        "ILE",
+        "MET",
+        "PHE",
+        "TRP",
+        "PRO",
+        "TYR"],
+    "disulphide": ["CYS"],
+    "ionic": [
+        "ARG",
+        "LYS",
+        "HIS",
+        "ASP",
+        "GLU"],
+    "aroaro": [
+        "PHE",
+        "TRP",
+        "TYR"],
+    "arosul": [
+        "PHE",
+        "TRP",
+        "TYR",
+        "CYS",
+        "MET"],
+    "cationpi": [
+        "LYS",
+        "ARG",
+        "PHE",
+        "TRP",
+        "TYR"],
+    "all": [
+        "ALA",
+        "GLY",
+        "ILE",
+        "LEU",
+        "PRO",
+        "VAL",
+        "PHE",
+        "TRP",
+        "TYR",
+        "ASP",
+        "GLU",
+        "ARG",
+        "HIS",
+        "LYS",
+        "SER",
+        "THR",
+        "CYS",
+        "MET",
+        "ASN",
+        "GLN"]}
+
+from math import sqrt
 from Bio.PDB import PDBParser
 parser=PDBParser(PERMISSIVE=1)
 structure=parser.get_structure(prot_id, prot_file)
 model=structure[0]
+
+
 for chain in model:
     for residue in chain:
         for atom in residue:
--- script/Script Aromatique Soufre.py
+++ script/Script Aromatique Soufre.py
@@ -12,23 +12,59 @@ parser = PDBParser(PERMISSIVE=1)
 structure = parser.get_structure(prot_id, prot_file)
 model = structure[0]
 
+
 achain = model['A']
 bchain = model['B']
-    
-ionicres = []
+
+
+arom_res = []
 for residue in achain:
-    if (residue.get_resname() == "HIS" or residue.get_resname() == "TRP" or residue.get_resname() == "TYR" or residue.get_resname() == "PHE"):
-        ionicres.append(residue)
-        #print(ionicres)
+    if (residue.get_resname() == "HIS" or residue.get_resname() == "TRP" or residue.get_resname() == "TYR" or residue.get_resname() == "PHE" or residue.get_resname() == "CYS" or residue.get_resname() == "MET"):
+        arom_res.append(residue)
         
-        for c1 in ionicres:
+        for c1 in arom_res:
             c1index = c1.get_id()[1]
             
-            for c2 in ionicres:
+            for c2 in arom_res:
                 c2index = c2.get_id()[1]
                 
-                if(c1['OE2'] - c2['N'] < 7 and c1['OE2'] - c2['N'] > 4.5):
-                    print("possible ionic bond:"),
+                if(c1['C'] - c2['S'] < 6 and c1['C'] - c2['S'] > 5):
+                    print("possible aromatic - sulfure interaction:"),
                     print (residue,c1index,"-")
                     print (residue,c2index,)
                     print (round(c1['OE2'] - c2['N'],2))
+                    
+                    
+                    
+                    
+
+
+
+def arosulfun(resid1, resid2, dist=5.3):
+    sulres = ["CYS", "MET"]
+    if (resid1.get_resname() in sulres and resid2.get_resname() not in sulres):
+        for atom in resid1:
+            if "S" in atom.get_name():
+                s_coord = atom.get_coord()
+                d = dist_cal(
+                    s_coord[0],
+                    s_coord[1],
+                    s_coord[2],
+                    resid2.center_mass[0],
+                    resid2.center_mass[1],
+                    resid2.center_mass[2])
+                if d < dist:
+                    return(d)
+    elif (resid2.get_resname() in sulres and resid1.get_resname() not in sulres):
+        for atom in resid2:
+            if "S" in atom.get_name():
+                s_coord = atom.get_coord()
+                d = dist_cal(
+                    s_coord[0],
+                    s_coord[1],
+                    s_coord[2],
+                    resid1.center_mass[0],
+                    resid1.center_mass[1],
+                    resid1.center_mass[2])
+                if d < dist:
+                    return(d)
