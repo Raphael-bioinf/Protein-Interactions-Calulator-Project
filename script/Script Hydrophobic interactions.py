4148080778225e63db35bc046b80d626532041ff
Raphael-bioinf <70586346+Raphael-bioinf@users.noreply.github.com>
2020-09-16 10:02:02 +0200

    Update Script Hydrophobic interactions.py

--- script/Script Hydrophobic interactions.py
+++ script/Script Hydrophobic interactions.py
@@ -14,7 +14,7 @@ structure=parser.get_structure(prot_id, prot_file)
 model=structure[0]
 
 parser=argparse.ArgumentParser(
-    description='''Ce programme identifie les liaisons cations-pi à partir d'un fichier Protein Data Bank (PDB). Les critères pris en compte proviennent du Protein Interaction Calculator que l'on peut retrouver en suivant le lien : http://pic.mbu.iisc.ernet.in/PIC_Criteria.pdf. Le module Biopython a été utilisé pour accéder à la structure des protéines. Le parser de Biopython est strucutré de la manière suivante : Structure/model/chain/residu/atome Fonctions utilisées :''',
+    description='''Ce programme identifie les interactions hydrophobes à partir d'un fichier Protein Data Bank (PDB). Les critères pris en compte proviennent du Protein Interaction Calculator que l'on peut retrouver en suivant le lien : http://pic.mbu.iisc.ernet.in/PIC_Criteria.pdf. Le module Biopython a été utilisé pour accéder à la structure des protéines. Le parser de Biopython est strucutré de la manière suivante : Structure/model/chain/residu/atome Fonctions utilisées :''',
     epilog="""Je vous souhaite une bonne utilisation du programme.""")
 parser.add_argument('parser.get_structure', type=int, default=42, help='Creation of a structure object from a PDB file')
 parser.add_argument('objet.get_name', type=int, default=43, help='Renvoie le nom correspondant à l objet : Structure/model/chain/residu/atome')
