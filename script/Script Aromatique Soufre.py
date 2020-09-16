09ac28df5df592788e91cc88da40f7b119ef57ac
Léo Maury <leomaury97@gmail.com>
2020-09-16 11:39:17 +0200

    pdb path to sys.argv

--- script/Script Aromatique Soufre.py
+++ script/Script Aromatique Soufre.py
@@ -4,12 +4,15 @@
 # In[1]:
 
 
-prot_id = "5AGY.pdb"
-prot_file = "../data/5AGY.pdb"
-
 from Bio.PDB import Atom
 from math import sqrt
 from Bio.PDB import PDBParser
+import argparse
+import sys
+
+
+prot_id = "5AGY.pdb"
+prot_file = sys.argv[1]
 
 
 parser = PDBParser(PERMISSIVE = 1)
@@ -21,8 +24,8 @@ parser = argparse.ArgumentParser(
     description = '''Ce programme identifie les interactions entre cycles aromatiques & atomes de soufre à partir d'un fichier Protein Data Bank (PDB). Les critères pris en compte proviennent du Protein Interaction Calculator que l'on peut retrouver en suivant le lien : http://pic.mbu.iisc.ernet.in/PIC_Criteria.pdf. Le module Biopython a été utilisé pour accéder à la structure des protéines. Le parser de Biopython est strucutré de la manière suivante : Structure/model/chain/residu/atome Fonctions utilisées :''',
     epilog = """Je vous souhaite une bonne utilisation du programme.""")
 parser.add_argument('parser.get_structure', type = int, default = 42, help = 'Creation of a structure object from a PDB file')
-parser.add_argument('objet.get_name', type = int, default = 43, help = 'Renvoie le nom correspondant à l objet : Structure/model/chain/residu/atome')
-parser.add_argument('atom.get_id', type = int, default = 44, help = 'Renvoie le numéro rattaché au résidue dans le fichier PDB')
+##parser.add_argument('objet.get_name', type = int, default = 43, help = 'Renvoie le nom correspondant à l objet : Structure/model/chain/residu/atome')
+##parser.add_argument('atom.get_id', type = int, default = 44, help = 'Renvoie le numéro rattaché au résidue dans le fichier PDB')
 args = parser.parse_args()
 
 
