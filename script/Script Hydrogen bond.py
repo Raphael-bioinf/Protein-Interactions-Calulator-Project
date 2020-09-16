915d3fa95f9bbea2746494e814f0f136705aec25
Léo Maury <leomaury97@gmail.com>
2020-09-16 18:45:15 +0200

    modified hbond_fun

--- script/Script Hydrogen bond.py
+++ script/Script Hydrogen bond.py
@@ -23,28 +23,26 @@ if "-h" in sys.argv or "--help" in sys.argv:
     print('')
 
 
-hydrogen = []
 residues = []
 for chain in model:
     for res in chain:
         residues.append(res)
 
     
-def hbondfun(acceptor, donnor, distON = 3.5, distS = 4):
+def hbond_fun(acceptor, donnor, resid1, resid2, distON = 3.5, distS = 4):
     d = acceptor - donnor
-    if "O" in acceptor.get_name() or "N" in acceptor.get_name():
-        if d < distON:
-            print(acceptor.get_name(), donnor.get_name(), "dist =", d)
-            return(d)
-    elif "S" in acceptor.get_name():
-        if d < distS:
-            print(acceptor.get_name(), donnor.get_name(), "dist =", d)
-            return(d)
+    if resid1.get_id()[1] != resid2.get_id()[1] and resid1.get_resname() != resid2.get_resname():
+        if "OH" in donnor.get_name() or "NH" in donnor.get_name() or "SG" in donnor.get_name():
+            if "O" in acceptor.get_name() or "N" in acceptor.get_name():
+                if d < distON:
+                    print(resid1.get_id()[1], resid1.get_resname(), acceptor.get_name(), resid2.get_id()[1], resid2.get_resname(), donnor.get_name(), "dist =", d)
+                    return(d)
+    elif resid1.get_id()[1] == resid2.get_id()[1]:
+        pass
 
 
-for res in residues:
-    for atom1 in res:
-        for atom2 in res:
-            hbondfun(atom1, atom2)
-
-
+for res1 in residues:
+    for res2 in residues:
+        for acceptor in res1:
+            for donnor in res2:
+                hbond_fun(acceptor, donnor, res1, res2)
